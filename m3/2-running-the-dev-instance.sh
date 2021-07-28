3aa65399a3748538bef2224bcb23f45483762903
ned1313 <ned.bellavance@gmail.com>
2021-07-28 13:30:04 -0400

    add token command

--- m3/2-running-the-dev-instance.sh
+++ m3/2-running-the-dev-instance.sh
@@ -6,6 +6,9 @@ vault server -dev -dev-root-token-id=86753098675309
 export VAULT_ADDR='http://127.0.0.1:8200'
 
 # Try the CLI
+# Check for the root token
+vault token lookup
+
 # List out the secrets engines
 vault secrets list
 
