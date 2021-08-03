154eacda54a2237502bfb36859d585f9bfba4c1c
ned1313 <ned.bellavance@gmail.com>
2021-08-03 09:35:44 -0400

    m5 azure vms

---
+++ m6/azure_vms/1-initialize_vault.sh
@@ -0,0 +1,40 @@
+# We are going to set up our local instance of the Vault executable to 
+# initialize and unseal the Vault server using PGP keys from our admins!
+
+#Install GnuPG and rng-tools
+sudo apt install gnupg rng-tools -y
+sudo rngd -r /dev/urandom
+
+#First we have to generate our pgp keys using gpg
+gpg --batch --gen-key vaultadmin1
+gpg --batch --gen-key vaultadmin2
+gpg --batch --gen-key vaultadmin3
+
+gpg --list-keys
+
+#Now we need the base64 encoded public keys to use with Vault
+gpg --export vaultadmin1 | base64 > vaultadmin1.asc
+gpg --export vaultadmin2 | base64 > vaultadmin2.asc
+gpg --export vaultadmin3 | base64 > vaultadmin3.asc
+
+#Now we can update the seal with our gpg keys
+export VAULT_ADDR="https://vault-test.globomantics.xyz:8200"
+vault operator init -key-shares=3 -key-threshold=2 -pgp-keys="vaultadmin1.asc,vaultadmin2.asc,vaultadmin3.asc"
+vault operator rekey -nonce NONCE_VALUE
+
+#Copy out the key values to key_shares.txt
+
+#Now seal the vault and unseal using the new key shares
+vault operator seal
+
+#Decrypt the first two keys
+echo "FIRST_KEY" | base64 --decode | gpg -u vaultadmin1 -dq
+echo "SECOND_KEY" | base64 --decode | gpg -u vaultadmin2 -dq
+
+#Unseal the vault
+vault operator unseal
+
+#Clean up the keys
+gpg --delete-secret-and-public-key vaultadmin1
+gpg --delete-secret-and-public-key vaultadmin2
+gpg --delete-secret-and-public-key vaultadmin3
